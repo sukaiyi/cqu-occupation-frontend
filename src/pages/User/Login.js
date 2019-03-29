7a16c720eadb270cf9e1bb3d3cf6bc69153aae43
sukaiyi <1433855681@qq.com>
2019-03-29 13:53:01 +0800

    基础权限框架

---
+++ src/pages/User/Login.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import { formatMessage, FormattedMessage } from 'umi-plugin-react/locale';
+import { Alert } from 'antd';
+import Login from '@/components/Login';
+import styles from './Login.less';
+
+const { Tab, UserName, Password, Submit } = Login;
+
+@connect(({ login, loading }) => ({
+  login,
+  submitting: loading.effects['login/login'],
+}))
+class LoginPage extends Component {
+  state = {
+    type: 'account',
+  };
+
+  onTabChange = type => {
+    this.setState({ type });
+  };
+
+  handleSubmit = (err, values) => {
+    if (!err) {
+      const { dispatch } = this.props;
+      dispatch({
+        type: 'login/login',
+        payload: values,
+      });
+    }
+  };
+
+  renderMessage = content => (
+    <Alert style={{ marginBottom: 24 }} message={content} type="error" showIcon />
+  );
+
+  render() {
+    const { login, submitting } = this.props;
+    const { type } = this.state;
+    return (
+      <div className={styles.main}>
+        <Login
+          defaultActiveKey={type}
+          onTabChange={this.onTabChange}
+          onSubmit={this.handleSubmit}
+          ref={form => {
+            this.loginForm = form;
+          }}
+        >
+          <Tab key="account" tab={formatMessage({ id: 'app.login.tab-login-credentials' })}>
+            {login.status === 'error' &&
+              login.type === 'account' &&
+              !submitting &&
+              this.renderMessage(formatMessage({ id: 'app.login.message-invalid-credentials' }))}
+            <UserName
+              name="username"
+              placeholder={`${formatMessage({ id: 'app.login.userName' })}: admin or user`}
+              rules={[
+                {
+                  required: true,
+                  message: formatMessage({ id: 'validation.userName.required' }),
+                },
+              ]}
+            />
+            <Password
+              name="password"
+              placeholder={`${formatMessage({ id: 'app.login.password' })}: ant.design`}
+              rules={[
+                {
+                  required: true,
+                  message: formatMessage({ id: 'validation.password.required' }),
+                },
+              ]}
+              onPressEnter={e => {
+                e.preventDefault();
+                this.loginForm.validateFields(this.handleSubmit);
+              }}
+            />
+          </Tab>
+          <Submit loading={submitting}>
+            <FormattedMessage id="app.login.login" />
+          </Submit>
+        </Login>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
